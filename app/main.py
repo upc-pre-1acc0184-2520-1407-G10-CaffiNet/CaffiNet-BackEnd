a8c43487bf6c12b279456cadcf417e7453e4a444
Carlo211 <carlopatricio4321@gmail.com>
2025-11-14 01:44:13 -0500

    feat: add search

--- app/main.py
+++ app/main.py
@@ -1,25 +1,23 @@
-# app/main.py - CÓDIGO FINAL CON CORRECCIÓN DE CORS
+# app/main.py
 from fastapi import FastAPI
-from starlette.middleware.cors import CORSMiddleware  # NECESARIO
+from fastapi.middleware.cors import CORSMiddleware
 from app.database import init_db
-import uvicorn
 
-# --- CONFIGURACIÓN DE FASTAPI ---
 app = FastAPI(title="Caffinet Backend")
 
-# Esto permite que tu frontend (Flutter Web) se comunique con el backend.
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Permite cualquier origen
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],  # Permite todos los métodos
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
 # Inicializa la base de datos y crea tablas si no existen
 init_db()
 
-# Routers (importa todos los que ya creamos)
+# Routers
 from app.routers import (
     cafeterias,
     tags,
@@ -33,7 +31,7 @@ from app.routers import (
     usuarios,
     favoritos,
     calificaciones,
-    historial_busquedas
+    historial_busquedas,
 )
 
 # Routers de datasets
@@ -42,21 +40,28 @@ app.include_router(cafeterias.router, prefix="/cafeterias", tags=["Cafeterias"])
 app.include_router(bebidas.router, prefix="/bebidas", tags=["Bebidas"])
 app.include_router(productos.router, prefix="/productos", tags=["Productos"])
 app.include_router(cafes.router, prefix="/cafes", tags=["Cafés"])
-app.include_router(cafeterias_bebidas.router, prefix="/cafeterias-bebidas", tags=["Cafeterias-Bebidas"])
-app.include_router(cafeterias_productos.router, prefix="/cafeterias-productos", tags=["Cafeterias-Productos"])
+app.include_router(
+    cafeterias_bebidas.router,
+    prefix="/cafeterias-bebidas",
+    tags=["Cafeterias-Bebidas"],
+)
+app.include_router(
+    cafeterias_productos.router,
+    prefix="/cafeterias-productos",
+    tags=["Cafeterias-Productos"],
+)
 app.include_router(horarios.router, prefix="/horarios", tags=["Horarios"])
 
 # Routers de usuarios y app
 app.include_router(auth.router, prefix="/auth", tags=["Auth"])
 app.include_router(usuarios.router, prefix="/usuarios", tags=["Usuarios"])
 app.include_router(favoritos.router, prefix="/favoritos", tags=["Favoritos"])
-app.include_router(calificaciones.router, prefix="/calificaciones", tags=["Calificaciones"])
+app.include_router(
+    calificaciones.router, prefix="/calificaciones", tags=["Calificaciones"]
+)
 app.include_router(historial_busquedas.router, prefix="/historial", tags=["Historial"])
+
 
 @app.get("/")
 def root():
     return {"message": "Backend Caffinet funcionando correctamente"}
-
-# --- BLOQUE DE EJECUCIÓN UVICORN ---
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
